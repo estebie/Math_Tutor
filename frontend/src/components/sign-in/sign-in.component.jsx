d79cede27567a2bb0796e956f308fa2a810ff6ac
ceniza.sjs <ceniza.sjs@sprobe.com>
2020-04-25 22:06:34 +0800

    Create sign in component.

---
+++ frontend/src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { 
+    Button,
+    Col,
+    Form, 
+    FormGroup,
+    InputGroup,  
+    InputGroupText, 
+    InputGroupAddon, 
+    Input,
+    Row
+ } from 'reactstrap';
+import './sign-in.styles.scss';
+
+const SignIn = () => (
+   <div>
+    <h3 className="text-center">Login</h3>
+    <br></br>
+    <Form>
+        <FormGroup row>
+            <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                    <InputGroupText>Email: </InputGroupText>
+                    </InputGroupAddon>
+                <Input type="email" name="email" id="email"/>
+            </InputGroup>
+        </FormGroup>
+        <FormGroup row>
+            <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                    <InputGroupText>Password: </InputGroupText>
+                    </InputGroupAddon>
+                <Input type="password" name="password" id="password"/>
+            </InputGroup>
+        </FormGroup>
+        <Row>
+            <Col sm={12}>
+                <p className="text-center text-md-right">Forgot your password?</p>
+            </Col>
+        </Row>
+        <Button outline color="primary" size="sm" block>Login</Button>
+        <Button outline color="danger" size="sm" block>Login with Google</Button>
+    </Form>
+   </div>
+)
+
+
+export default SignIn;
--- frontend/src/pages/login/login.component.jsx
+++ frontend/src/pages/login/login.component.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import './login.styles.scss';
 import logo from '../../assets/Math_Coach_Logo_With_Ops.png';
+import SignIn from '../../components/sign-in/sign-in.component';
 
 const LoginPage = () => (
-    <div className='login-container'>
-        <div className='form-container'>
-            <div className='logo-container' data-tilt data-tilt-glare="true" data-tilt-scale="1.1">
+    <div className='login-form-wrapper'>
+        <div className='login-form-container'>
+            <div className='logo-container'>
                 <img src={logo} alt="Logo" />
+            </div>
+            <div className='form-container'>
+            <SignIn></SignIn> 
             </div>
         </div>
     </div>
